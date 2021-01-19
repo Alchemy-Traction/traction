a53590e6adf0d81c7eb63877ac1d197b3967749b
Katie <katieberrym@gmail.com>
2021-01-18 18:25:40 -0800

    added habits table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS habits CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -7,4 +8,14 @@ CREATE TABLE users (
     password VARCHAR(20) NOT NULL,
     password_hash TEXT NOT NULL,
     user_photo_url TEXT NOT NULL
+);
+
+CREATE TABLE habits (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    prompt TEXT NOT NULL,
+    label TEXT NOT NULL,
+    reminder_type JSONB,
+    --day_of_week JSONB,
+    reminder_time INTEGER NOT NULL, 
+    user_id BIGINT REFERENCES users(id) NOT NULL
 );
