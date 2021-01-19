8b444df0cebf98bc17a4df67e493ee46e84d92d7
kadycat <katieberrym@gmail.com>
2021-01-18 18:41:54 -0800

    Sql/card 3 create habits table (#10)
    * added wireframe photos
    
    * added test to pass CI
    
    * created table users
    
    * added user table
    
    * added User model
    
    * added update and delete methods
    
    * added habits table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS habits CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,14 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     user_photo_url TEXT NOT NULL
 );
+
+CREATE TABLE habits (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    prompt TEXT NOT NULL,
+    label TEXT NOT NULL,
+    reminder_type JSONB,
+    --day_of_week JSONB,
+    reminder_time INTEGER NOT NULL, 
+    user_id BIGINT REFERENCES users(id) NOT NULL
+);
+
