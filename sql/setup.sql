acc7cf16e2eaeac71a78419e5874ec4999d644c9
Katie <katieberrym@gmail.com>
2021-01-18 16:07:00 -0800

    created table users

---
+++ sql/setup.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users --CASCADE;
+
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    phone_number INTEGER NOT NULL,
+    --phone_number_hash
+    password VARCHAR(20) NOT NULL,
+    password_hash TEXT NOT NULL,
+    user_photo_url TEXT NOT NULL
+)
