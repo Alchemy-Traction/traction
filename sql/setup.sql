cf356240a185d588f39ef22beb80fa69751e4168
Katie <katieberrym@gmail.com>
2021-01-18 17:47:07 -0800

    added update and delete methods

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     phone_number INTEGER NOT NULL,
-    --phone_number_hash
+    pin INTEGER NOT NULL,
     password VARCHAR(20) NOT NULL,
     password_hash TEXT NOT NULL,
     user_photo_url TEXT NOT NULL
